fd3bc854b5b34eb2ee6d60413d7da2c884a41fab
jsyeo97 <jsyeo97@naver.com>
2023-10-31 19:50:08 +0900

    chore: MySQL 사용하도록 수정

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Reservation;
-import com.example.demo.dto.ReservationDto;
 import com.example.demo.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
